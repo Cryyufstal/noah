7e8195aca12a4571edb516e7307ca31ed67eabe7
Cryyufstal <86175191+Cryyufstal@users.noreply.github.com>
2025-01-30 13:24:11 +0100

    Update tasks.ts

--- data/tasks.ts
+++ data/tasks.ts
@@ -2,6 +2,9 @@
 const tasks = [
   { id: 1, title: 'Visit Example Site', url: 'https://example.com', points: 10, completed: false },
   { id: 2, title: 'Check Blog Post', url: 'https://example.com/blog', points: 15, completed: false },
+  
+  { id: 4, title: 'Visit Example Site', url: 'https://example.com', points: 10, completed: false },
+  { id: 5, title: 'Check Blog Post', url: 'https://example.com/blog', points: 10, completed: false },
 ];
 
 export default tasks;
