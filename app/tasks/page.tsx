b174e8c967574c5895555715689ed589e9047f97
Cryyufstal <86175191+Cryyufstal@users.noreply.github.com>
2025-02-02 13:29:34 +0100

    Update page.tsx

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -179,7 +179,7 @@ const handleCompleteTask = async (id: number, points: number) => {
                 </button>
               ) : (
                 <button
-                  onClick={() => handleCompleteTask(task.id, task.points)}
+                  onClick={() => handleCompleteTask(task.id, task.points ?? 0)}
                   className="bg-green-500 hover:bg-green-600 text-white py-1 px-4 rounded transition-all"
                 >
                   Check
